3aa49363446ac34cca4f12b2bf1504471977dc8a
Paul Miller <paul@citywebconsultants.co.uk>
2020-07-06 14:22:08 +0100

    adding scaffolding for storing objects

--- backend/basic/index.js
+++ backend/basic/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
 const bp = require('body-parser');
+const  mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: process.env.MYSQLDBUSER,
+  password: process.env.MYSQLDBPASS,
+  database: process.env.MYSQLDBNAME
+});
+
+app.use(bp.json({ type: 'application/*+json' }))
 
 app.use('/key/:id', function (req, res, next) {
   //check formation of object
-  //add to db
-  console.log(req)
-  res.send('ahoy')
+  //res.send('ahoy+'+ req.params.id)
+  // write to db
+  console.log(req.body)
+
+  // check integrity of data against key
+  // chuck it in the db as text
   next();
 });
 
@@ -16,10 +29,20 @@ app.get('/', function (req, res) {
   res.send('Hello World. You are probable here because a URL broken. Try visiting...@tdoo');
 })
 
-
 const server = app.listen(8081, function () {
   const host = server.address().address
   const port = server.address().port
 
- console.log("Example app listening at http://%s:%s", host, port)
+ console.log("Example app listening at http://%s:%s", 'localhost', port)
 })
+
+//
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("1 record inserted");
+//   });
+// });
